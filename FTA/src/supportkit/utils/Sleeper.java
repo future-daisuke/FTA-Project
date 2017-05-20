071c71da2f67f7a581b9c2b55dd80f86158d7b16
Sato Hiroki <h.sato.j7@future.co.jp>
2017-05-20 12:21:18 +0900

    初期ワークスペース作成
    空のワークスペース追加しました

---
+++ FTA/src/supportkit/utils/Sleeper.java
@@ -0,0 +1,13 @@
+package supportkit.utils;
+
+public class Sleeper {
+
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// 無視する
+		}
+	}
+
+}
