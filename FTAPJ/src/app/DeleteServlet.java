785f208987b696633faf9379a43f2013ba2d6e54
AokiYohei <y.aoki.z5@future.co.jp>
2017-05-29 17:56:42 +0900

    完了と削除サーブレット

---
+++ FTAPJ/src/app/DeleteServlet.java
@@ -0,0 +1,49 @@
+package app;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/DeleteServlet")
+public class DeleteServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		System.out.println("post servlet in");
+		String taskId = request.getParameter("ID");
+		String status = request.getParameter("status");
+		int st = Integer.parseInt(status);
+		Date date = new Date();
+		//セッションの取得
+//		HttpSession session = request.getSession(true);
+//		session.setAttribute("comId", taskId)
+		String sql;
+		if(st == 3){
+			sql = "update TR_TASK set status_kbn = '1' ,delete_ymd = "+  date +" where ID = '"+ taskId +"'";
+		}else{
+			sql = "update TR_TASK set status_kbn = '3' ,delete_ymd = "+  date +" where ID = '"+ taskId +"'";
+		}
+		try(
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "fta", "fta");
+			// SQLの命令文を実行するための準備をおこないます
+			PreparedStatement stmt = con.prepareStatement(sql);
+		){
+			stmt.executeUpdate();
+			con.commit();
+
+			PrintWriter w = response.getWriter();
+			w.write(new ObjectMapper().writeValueAsString("success"));
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+}
