079561063d70f7cdf05225be6fcfa8e7b2af5c82
AokiYohei <y.aoki.z5@future.co.jp>
2017-06-05 22:05:00 +0900

    新規作成画面でカレンダーを常に表示、タイトルを20字制限、入力フォームが間違っていた時のアラートを表示するようにしました。

--- FTAPJ/WebContent/create.js
+++ FTAPJ/WebContent/create.js
@@ -2,12 +2,42 @@
  *
  */
 //期限フォームに今の日付を表示
-var dt = new Date();
-var y = dt.getFullYear();
-var m  = dt.getMonth() + 1;
-var d  = dt.getDate();
-//document.limit.dt.value = y+'年'+m+"月"+d+"日";
+ var date = new Date()
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
 
+  var toTargetDigits = function (num, digits) {
+    num += ''
+    while (num.length < digits) {
+      num = '0' + num
+    }
+    return num
+  }
+
+  var yyyy = toTargetDigits(year, 4)
+  var mm = toTargetDigits(month, 2)
+  var dd = toTargetDigits(day, 2)
+  console.log(yyyy+'-'+mm+'-'+dd)
+
+
+
+//ログイン認証
+function loginCheckAjax() {
+	'use strict';
+
+	$.ajax({
+		type : 'POST',
+		url : '/FTAPJ/api/LoginCheckServlet',
+		dataType : 'json',
+		success : function(json) {
+			if (json.syainId == null || json.syainId =="") {
+				// ログイン画面へ遷移
+				location.replace('/FTAPJ/indexLug.html');
+			}
+		}
+	});
+}
 
 
 
@@ -18,7 +48,7 @@ var d  = dt.getDate();
 		var title = document.getElementById('title').value;
 		var des = document.getElementById('description').value;
 		var submit = document.getElementById('submit').value;
-		var limit = document.getElementById('limit').value;
+		var limit = document.getElementById('display').value;
 		//取得したか確認
 		console.log(title);
 		console.log(des);
@@ -26,10 +56,10 @@ var d  = dt.getDate();
 		console.log(limit);
 		//データを指定
 		var requestQuery = {
-				title  : title,
-				description : des,
-				submit : submit,
-				limit : limit
+				title : escapeHTML(title),
+				description : escapeHTML(des),
+				submit : escapeHTML(submit),
+				limit : escapeHTML(limit),
 				};
 		console.dir(requestQuery);
 		$.ajax({
@@ -44,15 +74,33 @@ var d  = dt.getDate();
 				//取得した値を表示
 				console.log('post is success');
 				//成功したらリストページに遷移する
-				document.location.href = "/FTAPJ/index.jsp";
-			 }
+				document.location.href = "/FTAPJ/index.html";
+			 },  error: function(XMLHttpRequest, textStatus, errorThrown) {
+					//レスポンスがなければこちら
+				 	alert('入力文字数が多いか必須項目の入れ忘れです');
+				}
 
 		});
 	}
 
 
+//escape処理
+function escapeHTML(str) {
+		 str = str.replace(/&/g, '&amp;');
+		 str = str.replace(/</g, '&lt;');
+		 str = str.replace(/>/g, '&gt;');
+		 str = str.replace(/"/g, '&quot;');
+		 str = str.replace(/'/g, '&#39;');
+		 return str;
+	}
+
 $(document).ready(function() {
 	'use strict';
+
+	loginCheckAjax();
+
+	$('#limitform').append('<textarea id="display" rows="1" disabled>'+yyyy+'/'+mm+'/'+dd+'</textarea>');
+	$("#datepicker").datepicker("show");
 
 $("#submit_btn").bind('click',executeAjax);
 });
